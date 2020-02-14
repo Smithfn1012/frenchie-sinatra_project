a29faf67ebbf6ff1f5046a2fbc796967f445575c
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-02-14 11:53:22 -0700

    commit

---
+++ frenchie_sinatra_project/app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+    get '/login' do
+        erb :'/sessions/new'
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
+end
