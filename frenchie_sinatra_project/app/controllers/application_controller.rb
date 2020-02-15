e27cb4d39747c8c1b39ff18e47f86f8f09f46c42
Felicia_Smith <Smithfn1012@gmail.com>
2020-02-14 20:21:22 -0700

    debugging

--- frenchie_sinatra_project/app/controllers/application_controller.rb
+++ frenchie_sinatra_project/app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'mysupersillylongandcomplicatedstring'
   end
 
   get "/" do
@@ -19,6 +21,7 @@ class ApplicationController < Sinatra::Base
     !!session[:user_id]
   end
 
-  
-
+  def redirect_if_not_logged_in
+    redirect '/' if !is_logged_in?
+  end
 end
--- frenchie_sinatra_project/app/controllers/dogs_controller.rb
+++ frenchie_sinatra_project/app/controllers/dogs_controller.rb
@@ -1,14 +1,70 @@
 class DogsController < ApplicationController
+  post '/dogs' do
+      post = current_user.dogs.build(params[:dog])
+      if post.save
+        redirect '/dogs'
+      else
+        erb :'/dogs/new'
+      end
+  end
+  
     get '/dogs' do
       if is_logged_in?
         @dogs = current_user.dogs
+
         erb :'/dogs/index'
       else
         redirect '/login'
       end
     end
 
-    get '/'
+    get '/dogs/new' do
+      redirect_if_not_logged_in
+    
+      erb :'dogs/new'
+    end
 
+    get '/dogs/:id' do
+      redirect_if_not_logged_in
+      find_dog(params[:id])
 
+      erb :'dogs/show'
+    end
+
+    get '/dogs/:id/edit' do
+      redirect_if_not_logged_in
+      find_dog(params[:id])
+      authorize_user_to_dog(@dog)
+
+      erb :'dogs/edit'
+    end
+
+    patch '/dogs/:id' do
+      redirect_if_not_logged_in
+      find_dog(params[:id])
+      authorize_user_to_dog(@dog)
+
+      if @dog.update(params[:list])
+        redirect "/dogs/#{@dog.id}"
+      else
+        erb :'/dogs/edit'
+      end
+    end
+
+    delete '/dogs/:id' do
+      redirect_if_not_logged_in
+      find_dog(params[:id])
+      authorize_user_to_dog(@dog)
+
+      redirect '/dogs'
+    end
+
+    def find_dog(id)
+      @dog = Dog.find_by_id(params[:id])
+    end
+
+    def authorize_user_to_dog(dog)
+      redirect '/' unless dog.user_id == current_user.id
+    end
 end
+
